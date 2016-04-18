46eafad7ad40c7c2444a7050718671d90bb46e2a
nainirao <naineesharao@gmail.com>
2016-04-18 03:42:26 -0500

    commit

--- formEmail.php
+++ formEmail.php
@@ -3,13 +3,14 @@
   if (isset($_REQUEST['email']))  {
   
   //Email information
-  $admin_email = "someone@example.com";
-  $email = $_REQUEST['email'];
-  $subject = $_REQUEST['subject'];
-  $comment = $_REQUEST['comment'];
+  $admin_email = "naineeshadr@gmail.com";
+  $contactName= $_REQUEST['contactName'];
+  $contactEmail = $_REQUEST['contactEmail'];
+  $contactSubject = $_REQUEST['contactSubject'];
+  $contactMessage = $_REQUEST['contactMessage'];
   
   //send email
-  mail($admin_email, "$subject", $comment, "From:" . $email);
+  mail($admin_email, "$contactSubject", $contactMessage, "From:" . $contactEmail, $contactName);
   
   //Email response
   echo "Thank you for contacting us!";

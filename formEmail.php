d55e051b1f8f24d3d3a3dedbfc1724e23749a605
nainirao <naineesharao@gmail.com>
2016-04-19 14:44:52 -0500

    ping

--- formEmail.php
+++ formEmail.php
@@ -1,16 +1,16 @@
 <?php
 //if "email" variable is filled out, send email
-  if (isset($_REQUEST['email']))  {
+  if (isset($_REQUEST['contactForm']))  {
   
   //Email information
   $admin_email = "naineeshadr@gmail.com";
   $contactName= $_REQUEST['contactName'];
   $contactEmail = $_REQUEST['contactEmail'];
-  $contactSubject = $_REQUEST['contactSubject'];
+ 
   $contactMessage = $_REQUEST['contactMessage'];
   
   //send email
-  mail($admin_email, "$contactSubject", $contactMessage, "From:" . $contactEmail, $contactName);
+  mail($admin_email, $contactMessage, "From:" . $contactEmail, $contactName);
   
   //Email response
   echo "Thank you for contacting us!";
